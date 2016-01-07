371419745a934d7e1538107ae41e85ab6b3bd675
Lazybin <396887725@qq.com>
2016-01-07 21:07:15 +0800

    login

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -28,4 +29,7 @@ Route::get('/', function () {
 
 Route::group(['middleware' => ['web']], function () {
     //
+    Route::get('auth/login', 'Auth\AuthController@getLogin');
+    Route::post('auth/login', 'Auth\AuthController@postLogin');
+    Route::get('auth/logout', 'Auth\AuthController@getLogout');
 });
