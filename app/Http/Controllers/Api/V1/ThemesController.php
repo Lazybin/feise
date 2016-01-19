f7428e60fc8e2fb84795b042471c3752526c9004
Lazybin <a396887725@hotmail.com>
2016-01-19 21:56:17 +0800

    主题列表接口

---
+++ app/Http/Controllers/Api/V1/ThemesController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Model\BaseResponse;
+use App\Model\Themes;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+
+/**
+ * @SWG\Resource(
+ *     apiVersion="0.2",
+ *     swaggerVersion="1.2",
+ *     resourcePath="/themes",
+ *     basePath="http://120.27.199.121/feise/public/api/v1"
+ * )
+ */
+class ThemesController extends Controller
+{
+    /**
+     *
+     * @SWG\Api(
+     *   path="/themes/list",
+     *   description="主题",
+     *   @SWG\Operation(
+     *     method="GET", summary="获得主题列表", notes="获得主题列表",
+     *     type="Themes",
+     *     @SWG\ResponseMessage(code=0, message="成功"),
+     *     @SWG\Parameter(
+     *         name="start",
+     *         description="分页开始位置",
+     *         paramType="query",
+     *         required=false,
+     *         allowMultiple=false,
+     *         type="integer",
+     *         defaultValue=0
+     *     ),@SWG\Parameter(
+     *         name="length",
+     *         description="取得长度",
+     *         paramType="query",
+     *         required=false,
+     *         allowMultiple=false,
+     *         type="integer",
+     *         defaultValue=0
+     *     )
+     *
+     *   )
+     * )
+     */
+    public function index(Request $request)
+    {
+        $start=$request->input('start', 0);
+        $length=$request->input('length', 5);
+        $response=new BaseResponse();
+        $themes=Themes::skip($start)->take($length)->orderBy('id','desc');
+        $response->rows=$themes->get();
+        $response->total=Themes::count();
+        return $response->toJson();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Model/Themes.php
+++ app/Model/Themes.php
@@ -3,11 +3,22 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-
+/**
+ * @SWG\Model(id="Themes")
+ */
 class Themes extends Model
 {
+    /**
+     * @SWG\Property(name="category",type="string",description="主题分类")
+     * @SWG\Property(name="title",type="string",description="主题名称")
+     * @SWG\Property(name="category_id",type="integer",description="分类对应的id")
+     * @SWG\Property(name="cover",type="string",description="封面图片")
+     * @SWG\Property(name="head_image",type="string",description="页面顶部图片")
+     * @SWG\Property(name="type",type="integer",description="0-->普通模式,1-->图文结合模式")
+     * @SWG\Property(name="category",type="string",description="包含商品")
+     */
     protected $fillable = [
-        'category_id','title','category_id','cover','head_image','description','type'
+        'category_id','title','cover','head_image','description','type'
     ];
 
     protected $appends=['category','goods'];
