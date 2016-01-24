4fa9627e2789021238ae545771f1e3a3b8bdb23e
Lazybin <396887725@qq.com>
2016-01-24 16:15:29 +0800

    添加购物车

--- app/Http/Controllers/Api/V1/ShoppingCartController.php
+++ app/Http/Controllers/Api/V1/ShoppingCartController.php
@@ -96,36 +96,22 @@ class ShoppingCartController extends Controller
      *     method="POST", summary="添加到购物车", notes="添加到购物车",
      *     @SWG\ResponseMessage(code=0, message="成功"),
      *     @SWG\Parameter(
-     *         name="user_id",
-     *         description="用户id",
-     *         paramType="form",
+     *         name="newShoppingCartParams",
+     *         description="提交的商品信息",
+     *         paramType="body",
      *         required=true,
-     *         type="integer"
-     *     ),
-     *     @SWG\Parameter(
-     *         name="goods_id",
-     *         description="商品id",
-     *         paramType="form",
-     *         required=true,
-     *         allowMultiple=false,
-     *         type="integer"
-     *     ),@SWG\Parameter(
-     *         name="num",
-     *         description="数量",
-     *         paramType="form",
-     *         required=true,
-     *         allowMultiple=false,
-     *         type="integer"
+     *         type="newOrderParams"
      *     )
-     *
      *   )
      * )
      */
     public function store(Request $request)
     {
         $response=new BaseResponse();
-        $params=$request->all();
-        ShoppingCart::create($params);
+        $response=new BaseResponse();
+        $content = json_decode($request->getContent(false));
+        $content['properties'] =json_encode($content['properties']);
+        ShoppingCart::create($content);
         return $response->toJson();
     }
 
--- app/Model/ShoppingCart.php
+++ app/Model/ShoppingCart.php
@@ -3,6 +3,17 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @SWG\Model(
+ * id="newShoppingCartParams",
+ * @SWG\Property(name="goods_id",type="integer",description="商品id"),
+ * @SWG\Property(name="num",type="integer",description="购买数量"),
+ * @SWG\Property(name="user_id",type="integer",description="用户id"),
+ * @SWG\Property(name="properties",type="array",@SWG\Items("ordersGoodsProperties"),description="商品属性")
+ * )
+ */
+
 /**
  * @SWG\Model(id="ShoppingCart")
  */
@@ -15,7 +26,7 @@ class ShoppingCart extends Model
      * @SWG\Property(name="goods",type="Goods",description="商品详情")
      */
     protected $fillable = [
-        'user_id','goods_id','num'
+        'user_id','goods_id','num','properties'
     ];
 
     protected $appends=['goods'];
