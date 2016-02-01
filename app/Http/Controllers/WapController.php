296c9dd420763a741df05a154b3bbd92b65fe6c9
Lazybin <a396887725@hotmail.com>
2016-02-01 16:36:22 +0800

    1

--- app/Http/Controllers/WapController.php
+++ app/Http/Controllers/WapController.php
@@ -9,14 +9,62 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+/**
+ * @SWG\Resource(
+ *     apiVersion="0.2",
+ *     swaggerVersion="1.2",
+ *     resourcePath="/wap",
+ *     basePath="http://120.27.199.121/feise/public/"
+ * )
+ */
 class WapController extends Controller
 {
+    /**
+     *
+     * @SWG\Api(
+     *   path="/wap/banner_detail/{id}",
+     *   description="APP WAP 页",
+     *   @SWG\Operation(
+     *     method="GET", summary="banner详情wap", notes="banner详情wap",
+     *     type="Goods",
+     *     @SWG\ResponseMessage(code=0, message="成功"),
+     *     @SWG\Parameter(
+     *         name="id",
+     *         description="banner id",
+     *         paramType="path",
+     *         required=true,
+     *         allowMultiple=false,
+     *         type="integer",
+     *     )
+     *
+     *   )
+     * )
+     */
     public function bannerDetail($id){
         $data['content']=Banner::find($id)->toArray();
         return view('wap.banner_detail',$data);
     }
 
-
+    /**
+     *
+     * @SWG\Api(
+     *   path="/wap/goods_detail/{id}",
+     *   @SWG\Operation(
+     *     method="GET", summary="商品详情wap", notes="商品详情wap",
+     *     type="Goods",
+     *     @SWG\ResponseMessage(code=0, message="成功"),
+     *     @SWG\Parameter(
+     *         name="id",
+     *         description="商品 id",
+     *         paramType="path",
+     *         required=true,
+     *         allowMultiple=false,
+     *         type="integer",
+     *     )
+     *
+     *   )
+     * )
+     */
     public function goodsDetail($id){
         $data['goods']=Goods::find($id)->toArray();
         //echo $data['goods']['detailed_introduction'];exit;
