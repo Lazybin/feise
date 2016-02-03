ca33e39ee0b8ee88845a0508355ead4fef2e3490
Lazybin <a396887725@hotmail.com>
2016-02-03 11:21:38 +0800

    1

--- app/Model/Collection.php
+++ app/Model/Collection.php
@@ -15,16 +15,26 @@ class Collection extends Model
      * @SWG\Property(name="user_id",type="integer",description="用户id")
      * @SWG\Property(name="type",type="integer",description="类型，0---》商品 1---》主题")
      * @SWG\Property(name="item_id",type="integer",description="项id")
-     * @SWG\Property(name="item",type="integer",description="收藏的项，type=0时为商品，1时为主题")
+     * @SWG\Property(name="goods_item",type="Goods",description="收藏的商品，type=0时有值")
+     * @SWG\Property(name="theme_item",type="Themes",description="收藏的主题，type=1时有值")
      */
-    protected $appends=['item'];
+    protected $appends=['goods_item','theme_item'];
 
-    public function getItemAttribute()
+    public function getGoodsItemAttribute()
+    {
+        if($this->type!=0){
+            return null;
+        }else{
+            return Goods::find($this->item_id)->toArray();
+        }
+    }
+
+    public function getThemeItemAttribute()
     {
         if($this->type!=0){
             return Themes::find($this->item_id)->toArray();
         }else{
-            return Goods::find($this->item_id)->toArray();
+            return null;
         }
     }
 }
