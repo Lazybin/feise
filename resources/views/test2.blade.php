ad47111ae948334630df9cc02aca5ec989d90a61
Lazybin <a396887725@hotmail.com>
2016-02-04 17:18:18 +0800

    1

--- resources/views/test2.blade.php
+++ resources/views/test2.blade.php
@@ -19,22 +19,6 @@
         li{list-style: none;}
         p.img{width: 100%;}
 
-        @media screen and (max-width: 580px){
-            body{font-size: 14px;}
-            .itemlist li{font-size: 1em;}
-            .itemlist p.text1{font-size: 1.2em;}
-        }
-        @media screen and (max-width: 520px){
-            body{font-size: 12px;}
-            .itemlist li{font-size: 1em;}
-            .itemlist p.text1{font-size: 1.2em;}
-
-
-        }
-        @media screen and (max-width: 425px){
-            p.text{ line-height: 1.5em; font-size: 1em; }
-            .itemlist p.text2{font-size: 1.2em;width: 90%; margin-left: 5%;}
-        }
 
         .thumb{
             position:relative;
@@ -129,27 +113,89 @@
 
         .zhufubiaoti{left:58%;top:70%;position:absolute;width: 2%;float: left;line-height: 1.1em; font-size:1.5em;font-weight:600;color: #FCCDA3;}
         .zhuwenzi{left:45%;top:70%;position:absolute;width: 2%;float: left;line-height: 1.2em; font-size:1.8em;font-weight:700;color: #FCCDA3;}
-        .tishi1{line-height: 1em; font-size:1.4em;color: #FCCDA3;}
-        .tishi2{line-height: 1em; font-size:1.2em;color: #FCCDA3;}
+
+
+        @media screen and (max-width: 580px){
+            body{font-size: 14px;}
+            .itemlist li{font-size: 1em;}
+            .itemlist p.text1{font-size: 1.2em;}
+            .tishi1{line-height: 1em; font-size:3em;color: #FCCDA3;}
+            .tishi2{line-height: 1em; font-size:2em;color: #FCCDA3;}
+        }
+        @media screen and (max-width: 520px){
+            body{font-size: 12px;}
+            .itemlist li{font-size: 1em;}
+            .itemlist p.text1{font-size: 1.2em;}
+            .tishi1{line-height: 1em; font-size:1.4em;color: #FCCDA3;}
+            .tishi2{line-height: 1em; font-size:1.2em;color: #FCCDA3;}
+
+            #tishi1{margin: auto;left:0;right:0;bottom:-190%;position:absolute;display: none}
+            #tishi2{margin: auto;left:0;right:0;bottom:-205%;position:absolute;display: none}
+
+            .zhufubiaoti{
+                left:57%;top:70%;
+                position:absolute;
+                width: 2%;float: left;line-height: 1em;
+                font-size:1.8em;font-weight:600;color: #FCCDA3;
+            }
+
+            .zhuwenzi{
+                left:47%;top:70%;
+                position:absolute;width: 2%;
+                float: left;line-height: 1.2em;
+                font-size:2em;font-weight:700;
+                color: #FCCDA3;
+            }
+
+
+        }
+        @media screen and (max-width: 320px){
+            p.text{ line-height: 1.5em; font-size: 1em; }
+            .itemlist p.text2{font-size: 1.2em;width: 90%; margin-left: 5%;}
+            .tishi1{line-height: 1em; font-size:1.2em;color: #FCCDA3;}
+            .tishi2{line-height: 1em; font-size:1em;color: #FCCDA3;}
+
+            #tishi1{margin: auto;left:0;right:0;bottom:-190%;position:absolute;display: none}
+            #tishi2{margin: auto;left:0;right:0;bottom:-205%;position:absolute;display: none}
+
+            .zhufubiaoti{
+                left:56%;top:65%;
+                position:absolute;
+                width: 2%;float: left;line-height: 1em;
+                font-size:1.6em;font-weight:600;color: #FCCDA3;
+            }
+
+            .zhuwenzi{
+                left:47%;top:65%;
+                position:absolute;width: 2%;
+                float: left;line-height: 1.2em;
+                font-size:1.8em;font-weight:700;
+                color: #FCCDA3;
+            }
+        }
+
     </style>
 </head>
 <body style="background-color:#AD1929 ">
 <section style="position:relative;text-align: center">
     <p id="head" style="position:relative;"  class="img show"><img src="img/first_head.png" alt=""></p>
     <p id="aaaa" class="img base_pos test5 hidden" style="display: none"><img src="img/open_background.png" alt=""></p>
-    <p class="img" style="position: absolute;bottom: -145%;"><img id="img_box" class=""  style="width:45%" onclick="onOpenClick()" src="img/first_box.png" alt=""></p>
-    <p id="title" style="display: none"><span class="zhufubiaoti">恭祝您</span></p>
-    <p id="content" style="display: none"><span class="zhuwenzi">运筹帷幄事业新</span></p>
-    <p id="tishi1" style="margin: auto;left:0;right:0;bottom:-175%;position:absolute;"><span class="tishi1">已找到今日宝藏，还有7天，请再接再厉！</span></p>
-    <p id="tishi2" style="margin: auto;left:0;right:0;bottom:-190%;position:absolute;"><span class="tishi2">要连续点击七天才能得大礼哦！</span></p>
-    <p id="p_thumb" class="img" style="position: absolute;bottom: -200%;"><img class="thumb" onclick="onOpenClick()" style="width:15%" src="img/thumb.png" alt=""></p>
+    <p class="img" style="position: absolute;bottom: -145%;">
+        <img id="img_box" class=""  style="width:45%" onclick="onOpenClick()" src="img/first_box.png" alt="">
+    </p>
+    <p id="title" style="display: none;"><span class="zhufubiaoti">恭祝您</span></p>
+    <p id="content" style="display: none;"><span class="zhuwenzi">运筹帷幄事业新</span></p>
+    <p id="tishi1"><span class="tishi1">已找到今日宝藏，还有7天，请再接再厉！</span></p>
+    <p id="tishi2"><span class="tishi2">要连续点击七天才能得大礼哦！</span></p>
+    <p id="p_thumb" class="img" style="position: absolute;bottom: -200%;">
+        <img class="thumb" onclick="onOpenClick()" style="width:15%" src="img/thumb.png" alt="">
+    </p>
 </section>
 <script src="{{ url('../resources/assets/vendor/jquery/dist/jquery.min.js') }}"></script>
 <script type="text/javascript">
     $(document).ready(function() {
     });
     function onOpenClick(){
-        //alert(11);
         $("#aaaa").removeClass("hidden").addClass("show");
         $("#aaaa").css('display','inline');
         $("#head").removeClass("show").addClass("hidden");
@@ -161,6 +207,8 @@
             $("#img_box").attr('src','img/box_open.png');
             $("#content").css('display','inline');
             $("#title").css('display','inline');
+            $("#tishi1").css('display','inline');
+            $("#tishi2").css('display','inline');
             //e.target.style.display="none";
         },false);
     }
