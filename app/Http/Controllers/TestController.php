2c7dc6c6402765fa9685eef9db3baaddbfb5e7ea
Lazybin <a396887725@hotmail.com>
2016-02-04 14:01:16 +0800

    春节活动测试

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -16,7 +16,7 @@ class TestController extends Controller
 
     public function index(){
 
-        return view('test');
+        return view('test1');
     }
 
     public function bannerDetail(){
---
+++ resources/views/test1.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>活动详情</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="format-detection" content="telephone=no">
+    <meta http-equiv="Expires" content="-1">
+    <meta http-equiv="Cache-Control" content="no-cache">
+    <meta http-equiv="Pragma" content="no-cache">
+    <!--<link rel="apple-touch-icon-precomposed" href="http://st.360buyimg.com/m/images/apple-touch-icon.png?v=jd2015030537">-->
+    <style type="text/css">
+        body,div,dl,dt,dd,ul,ol,li,h1,h2,h3,h4,h5,h6,pre,form,label,fieldset,input,textarea,p,blockquote,th,td,section,canvas{margin: 0px; padding: 0px;}
+        img{margin: 0px; padding:0px; border:none;width: 100%; }
+        body{font-family: 'Microsoft YaHei', Arial;  font-size: 16px; min-width: 320px;}
+        section{max-width: 640px;margin: 0px auto;}
+        li{list-style: none;}
+        p.img{width: 100%;}
+
+        @media screen and (max-width: 580px){
+            body{font-size: 14px;}
+            .itemlist li{font-size: 1em;}
+            .itemlist p.text1{font-size: 1.2em;}
+        }
+        @media screen and (max-width: 520px){
+            body{font-size: 12px;}
+            .itemlist li{font-size: 1em;}
+            .itemlist p.text1{font-size: 1.2em;}
+
+
+        }
+        @media screen and (max-width: 425px){
+            p.text{ line-height: 1.5em; font-size: 1em; }
+            .itemlist p.text2{font-size: 1.2em;width: 90%; margin-left: 5%;}
+        }
+    </style>
+</head>
+<body style="background-color:#AD1929 ">
+<section style="position:relative;text-align: center">
+    <p class="img"><img src="img/first_head.png" alt=""></p>
+    <p class="img" style="position:absolute;left:0px;top:0px;z-index:-1"><img src="img/first_background.png" alt=""></p>
+    <p class="img" style="margin-top: 25em;"><img style="width:30%" src="img/first_box.png" alt=""></p>
+
+
+</section>
+</body>
+</html>
