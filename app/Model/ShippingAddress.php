268aee56a22b4ff7cd46896565d4c7f2e0fdd027
Lazybin <a396887725@hotmail.com>
2016-02-16 11:14:55 +0800

    1

--- app/Model/ShippingAddress.php
+++ app/Model/ShippingAddress.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Model;
  * @SWG\Property(name="user_id",type="integer",description="用户id"),
  * @SWG\Property(name="consignee",type="string",description="联系人"),
  * @SWG\Property(name="mobile",type="string",description="联系电话"),
- * @SWG\Property(name="is_default",type="string",description="是否默认 0-->否，1--》是"),
+ * @SWG\Property(name="is_default",type="integer",description="是否默认 0-->否，1--》是"),
  * @SWG\Property(name="province",type="string",description="省名称"),
  * @SWG\Property(name="city",type="string",description="市名称"),
  * @SWG\Property(name="district",type="string",description="区\县名称"),
@@ -27,14 +27,14 @@ class ShippingAddress extends Model
      * @SWG\Property(name="user_id",type="integer",description="用户id")
      * @SWG\Property(name="consignee",type="string",description="联系人")
      * @SWG\Property(name="mobile",type="string",description="联系电话")
-     * @SWG\Property(name="is_default",type="string",description="是否默认 0-->否，1--》是")
+     * @SWG\Property(name="is_default",type="integer",description="是否默认 0-->否，1--》是")
      * @SWG\Property(name="province",type="string",description="省")
      * @SWG\Property(name="city",type="string",description="市")
      * @SWG\Property(name="district",type="string",description="区\县")
      * @SWG\Property(name="detailed_address",type="string",description="详细地址")
      */
     protected $fillable = [
-        'user_id','province','city','district','detailed_address'
+        'user_id','consignee','mobile','is_default','province','city','district','detailed_address'
     ];
 
 }
