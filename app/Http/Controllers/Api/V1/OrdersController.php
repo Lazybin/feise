d6b4aee246d17e72d613ef14b216a127150613d0
Lazybin <a396887725@hotmail.com>
2016-02-21 15:02:21 +0800

    1

--- app/Http/Controllers/Api/V1/OrdersController.php
+++ app/Http/Controllers/Api/V1/OrdersController.php
@@ -521,7 +521,7 @@ class OrdersController extends Controller
 
         }
         $response->Data=$data;
-        $response->rows=[];
+        $response->rows=null;
         return $response->toJson();
     }
     public function getWeiXinPayParameter($prepay_id)
