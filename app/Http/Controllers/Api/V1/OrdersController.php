272355f70fad9d1f4989a8f217115b1ea3627b68
Lazybin <a396887725@hotmail.com>
2016-02-21 13:50:08 +0800

    银联支付

--- app/Http/Controllers/Api/V1/OrdersController.php
+++ app/Http/Controllers/Api/V1/OrdersController.php
@@ -462,7 +462,63 @@ class OrdersController extends Controller
             }
             $data['weixinPayInfo']=$this->getWeiXinPayParameter($t['responseObj']->prepay_id);
         }else{
-            $data['payInfo']=null;
+            include_once '../vendor/yinlian_sdk/acp_service.php';
+
+            $params = array(
+
+                //以下信息非特殊情况不需要改动
+                'version' => '5.0.0',                 //版本号
+                'encoding' => 'utf-8',				  //编码方式
+                'txnType' => '01',				      //交易类型
+                'txnSubType' => '01',				  //交易子类
+                'bizType' => '000201',				  //业务类型
+                'frontUrl' =>  SDK_FRONT_NOTIFY_URL,  //前台通知地址
+                'backUrl' => SDK_BACK_NOTIFY_URL,	  //后台通知地址
+                'signMethod' => '01',	              //签名方法
+                'channelType' => '08',	              //渠道类型，07-PC，08-手机
+                'accessType' => '0',		          //接入类型
+                'currencyCode' => '156',	          //交易币种，境内商户固定156
+
+                //TODO 以下信息需要填写
+                'merId' => '777290058125654',		//商户代码，请改自己的测试商户号，此处默认取demo演示页面传递的参数
+                'orderId' =>$out_trade_no,	//商户订单号，8-32位数字字母，不能含“-”或“_”，此处默认取demo演示页面传递的参数，可以自行定制规则
+                'txnTime' => date('YmdHis',time()),	//订单发送时间，格式为YYYYMMDDhhmmss，取北京时间，此处默认取demo演示页面传递的参数
+                'txnAmt' => 1,	//交易金额，单位分，此处默认取demo演示页面传递的参数
+// 		'reqReserved' =>'透传信息',        //请求方保留域，透传字段，查询、通知、对账文件中均会原样出现，如有需要请启用并修改自己希望透传的数据
+
+                //TODO 其他特殊用法请查看 pages/api_05_app/special_use_purchase.php
+            );
+
+
+            \AcpService::sign ( $params ); // 签名
+            $url = SDK_App_Request_Url;
+
+            $result_arr = \AcpService::post ($params,$url);
+            if(count($result_arr)<=0) { //没收到200应答的情况
+                $response->Code=BaseResponse::CODE_ERROR_CHECK;
+                $response->Message='服务器内部错误，错误码：108';
+                return $response->toJson();
+            }
+
+            //printResult ($url, $params, $result_arr ); //页面打印请求应答数据
+
+            if (!\AcpService::validate ($result_arr) ){
+                $response->Code=BaseResponse::CODE_ERROR_CHECK;
+                $response->Message='应答报文验签失败';
+                return $response->toJson();
+            }
+
+
+            if ($result_arr["respCode"] == "00"){
+                //成功
+                //TODO
+                $data['yinlianPayInfo']=$result_arr["tn"];
+            } else {
+                $response->Code=BaseResponse::CODE_ERROR_CHECK;
+                $response->Message='服务器内部错误，错误码：108';
+                return $response->toJson();
+            }
+
         }
         $response->Data=$data;
         return $response->toJson();
