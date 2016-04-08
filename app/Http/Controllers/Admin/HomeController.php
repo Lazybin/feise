db39fa455ce0661051ff9ff16176fd717e05d3f4
Lazybin <a396887725@hotmail.com>
2016-04-08 21:18:57 +0800

    1

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -12,6 +12,7 @@ class HomeController extends Controller
     //
     public function index()
     {
-        return view('admin.home.index');
+        return redirect()->action('Admin\HomeManageController@show');
+     //   return view('admin.home.index');
     }
 }
